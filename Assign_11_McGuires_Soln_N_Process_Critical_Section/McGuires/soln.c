30ea387b756b865f34ab5e867f39dcfc09278e66
Ayush Jain <jainayush4you@gmail.com>
2020-03-29 17:17:33 +0530

    McGuires Solution for N-process critical section

---
+++ Assign_11_McGuires_Soln_N_Process_Critical_Section/McGuires/soln.c
@@ -0,0 +1,123 @@
+// Implementation of McGuires solution for N-process
+// critical section
+// 2017UCP1168
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+
+#define BUFSIZE 1
+#define PERMS 0666 //0666 - To grant read and write permissions
+
+const int N = 6;
+const int loopMax = 200;
+enum Status {IDLE, WANT_IN, IN_CS};
+
+double elapsed;
+int x = 2;
+int *flag;
+int *turn = 0;
+int *counter = 0;
+clock_t start, stop;
+
+void Process(int i) {
+	int j, index = 0;
+	printf("Child: %d began running!\n", i);
+	sleep(3);
+	while(index < loopMax) {
+		while (1) {
+			*(flag+i) = WANT_IN;
+			j = *turn;
+			while (j != i) {
+				if (*(flag+j) != IDLE)
+					j = *turn;
+				else
+					j = (j + 1) % N;
+			}
+
+			*(flag+i) = IN_CS;
+			j = 0;
+
+			while ((j < N) && ((j == i) || (*(flag+j) != IN_CS))) {
+				j = j + 1;
+			}
+
+			if((j >= N) && (*turn == i || (*(flag+*turn) == IDLE)))
+				break;
+		}
+		*turn = i;
+
+		/* CRITICAL SECTION */
+		stop = clock();
+		elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+		printf("Process %d took %f milliseconds to enter the CS!\n", i, elapsed);
+		printf("Process %d with index %d\n", i, index+1);
+		printf("Process %d ", i);
+		printf(" has entered the critical section and went to sleep!\n");
+		sleep(1);
+		printf("Process %d ", i);
+		*counter = *counter + i;
+		printf(" has woke up and updated the counter to %d \n", *counter);
+
+		/* END OF CRITICAL SECTION */
+		start = clock();
+
+		j = (*turn + 1) % N;
+		while (*(flag+j) == IDLE) {
+			j = (j + 1) % N;
+		}
+			*turn = j;
+			*(flag+i) = IDLE;
+		index++;
+	}
+}
+
+int main(void) {
+	int i, pid, shmid;
+
+	/* Initialize Shared Memory */
+	counter = mmap(NULL, sizeof *counter, PROT_READ | PROT_WRITE,
+                    MAP_SHARED | MAP_ANON, -1, 0);
+	*counter = 0;
+	turn = mmap(NULL, sizeof *turn, PROT_READ | PROT_WRITE,
+                    MAP_SHARED | MAP_ANON, -1, 0);
+	*turn = 0;
+
+	if((shmid = shmget(1000, BUFSIZE, IPC_CREAT | PERMS)) < 0) {
+		printf("\n unable to create shared memory");
+ 		return 0;
+	}
+
+	/* Initialize Shared Array */
+	if((flag=(int*)shmat(shmid,(char*)0,0)) == (int*)-1) {
+  		printf("\n Shared memory allocation error\n");
+  		exit(1);
+	}
+
+	int loopIndex;
+	for(loopIndex = 0; loopIndex < N; loopIndex++)
+		*(flag+loopIndex) = IDLE;
+
+	int child;
+	for (child = 0; child < N; child++) {
+    	pid = fork();
+    	if (pid)
+        	continue;
+    	else if (pid == 0) {
+    		Process(child);
+        	break;
+    	} else {
+        	printf("Fork error!\n");
+        	exit(1);
+    	}
+	}
+
+	return 0;
+}
---
+++ Assign_11_McGuires_Soln_N_Process_Critical_Section/Searchers_Inserters_Deleters/pro.c
@@ -0,0 +1,198 @@
+// Ayush Jain - 2017UCP1168
+// Searchers - Inserters - Deleters
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <math.h>
+#include <time.h>
+#include <errno.h>
+#include <semaphore.h>
+
+#define N 3
+#define NS 10
+typedef enum{ INSERT, SEARCH, DELETE } checker_t;
+typedef enum{ ON, OFF } state;
+
+sem_t sem_search;
+sem_t sem_delete;
+sem_t sem_insert;
+state state_delete = OFF;
+state state_insert = OFF;
+state state_search = OFF;
+
+int delete_ID[N] = { 0, 1, 2 };
+int insert_ID[N] = { 0, 1, 2 };
+int search_ID[NS];
+
+int search_count = 0;
+int insert_count = 0;
+int delete_count = 0;
+
+unsigned int fib(unsigned int n) {
+	return n < 2 ? n : fib(n - 1) + fib(n - 2);
+}
+
+void check(checker_t checker) { /* My check function */
+	switch (checker) {
+	case INSERT:
+		if (state_delete == OFF && state_insert == OFF) {            /* Inserter can only work with search. */
+			sem_post(&sem_insert);
+		}
+		break;
+
+	case SEARCH:                          /* Searcher can work parallelly with search and insert, but cannot work with delete. */
+		if (state_delete == OFF) {
+			sem_post(&sem_search);
+		}
+		break;
+
+	case DELETE:						  /* Deleter can work with nobody. */
+		if (state_insert == OFF && search_count == 0) {
+			sem_post(&sem_delete);
+		}
+		break;
+	}
+}
+
+void* searcher(void* id) {
+	int *ID = (int*)id;
+
+	while (1) {
+		int sleep_time = (rand() % 5 >= 0) ? (rand() % 5) : -(rand() % 5);
+		sleep(sleep_time);
+
+		printf("[SEARCHER %d] WAKE UP \n"
+			"[THREADS] %d SEARCH | %d INSERT | %d DELETE \n\n", *ID, search_count, insert_count, delete_count);
+		if(state_delete == OFF)
+			sem_post(&sem_search);
+		else
+			printf("[SEARCHER %d] WAIT FOR DELETER \n\n", *ID);
+
+		sem_wait(&sem_search);
+		printf("[SEARCHER %d] SAFE FOR SEARCH \n"
+			"[THREADS] %d SEARCH | %d INSERT | %d DELETE \n\n", *ID, search_count, insert_count, delete_count);
+
+		search_count++;
+
+		/* Search for a random number. */
+		printf("[SEARCHER %d] LOOKING UP\n"
+			"[THREADS] %d SEARCH | %d INSERT | %d DELETE \n\n", *ID, search_count, insert_count, delete_count);
+
+		fib(20);
+
+		search_count--;
+		check(DELETE);
+	}
+
+	return NULL;
+}
+
+
+void* deleter(void* id) {
+	int* ID = (int*)id;
+
+	while (1) {
+		int sleep_time = ((rand() % 10) >= 0) ? (rand() % 10) : -(rand() % 10); /*Generate random number*/
+		sleep(sleep_time);
+
+		printf("[DELETER %d] WAKE UP \n"
+			"[THREADS] %d SEARCH | %d INSERT | %d DELETE \n\n",*ID, search_count, insert_count, delete_count);
+
+		if(search_count == 0 && state_insert == OFF) {
+			sem_post(&sem_delete);
+			pthread_mutex_unlock(&sem_delete);
+		}
+		else
+			printf("[DELETER %d] WAIT FOR SEARCHER OR INSERTER \n \n", *ID);
+
+		sem_wait(&sem_delete);
+		state_delete = ON;
+		delete_count++;
+
+		/* Delete a random number. */
+		printf("[DELETER %d] DELETE ONE ITEM \n"
+			"[THREADS] %d SEARCH | %d INSERT | %d DELETE \n\n", *ID, search_count, insert_count, delete_count);
+
+		fib(20);
+
+		delete_count--;
+		state_delete = OFF;
+
+		check(INSERT);
+		check(SEARCH);
+	}
+
+	return NULL;
+}
+
+void* inserter(void* id) {
+	int *ID = (int*)id;
+
+	while (1) {
+		int sleep_time = ((rand() % 5) >= 0) ? (rand() % 10) : -(rand() % 10); /*Generate random number*/
+		sleep(sleep_time);
+
+		printf("[INSERTER %d] WAKE UP \n"
+			"[THREADS] %d SEARCH | %d INSERT | %d DELETE \n\n", *ID, search_count, insert_count, delete_count);
+
+		if (state_insert == OFF && state_delete == OFF)
+			sem_post(&sem_insert);
+		else
+			printf("[INSERTER %d] WAIT FOR OTHER INSERTER OR DELETER \n\n", *ID);
+
+		sem_wait(&sem_insert);
+		state_insert = ON;
+		insert_count++;
+
+		printf("[INSERTER %d] ADD A NEW \n"
+			"[THREADS] %d SEARCH | %d INSERT | %d DELETE \n\n", *ID, search_count, insert_count, delete_count);
+		fib(20);
+
+		insert_count--;
+		state_insert = OFF;
+
+		check(DELETE);
+		check(INSERT);
+	}
+
+	return NULL;
+}
+
+int main(int argc, char *argv[]) {
+	pthread_t inst[N];
+	pthread_t srch[NS];
+	pthread_t delt[N];
+
+	sem_init(&sem_search, 0, 0);
+	sem_init(&sem_insert, 0, 0);
+	sem_init(&sem_delete, 0, 0);
+
+	for(int i = 0; i < NS; i++)
+		search_ID[i] = i;
+
+	/* Threads Activation */
+	for (int i = 0; i < N; i++) {
+		int pthread_create_err = pthread_create(&inst[i], NULL, inserter, (void*)&insert_ID[i]);
+		if (pthread_create_err != 0)	/* When initialize the thread unsuccessfully. */
+			perror("pthread_create()");
+
+		pthread_create_err = pthread_create(&delt[i], NULL, deleter, (void*)&delete_ID[i]);
+		if (pthread_create_err != 0)	/* When initialize the thread unsuccessfully. */
+			perror("pthread_create()");
+	}
+
+	for(int i = 0; i < NS; i++) {
+		int pthread_create_err = pthread_create(&srch[i], NULL, searcher, (void*)&search_ID[i]);
+		if(pthread_create_err != 0)
+			perror("pthread_create()");
+	}
+
+	for (int i = 0; i < N; i++) {
+		pthread_join(inst[i], NULL);
+		pthread_join(delt[i], NULL);
+	}
+
+	for(int i = 0; i < NS; i++)
+		pthread_join(srch[i], NULL);
+}
